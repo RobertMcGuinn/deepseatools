c2eba3b845045cffa5029165637498b0028b8247
robert.mcguinn <robert.mcguinn@noaa.gov>
2020-04-16 13:46:19 -0400

    obis release changes for Q2 2020

--- code/release_to_obis.R
+++ code/release_to_obis.R
@@ -1,25 +1,42 @@
 ##### Header #####
-# original author: Matt Dornback
-# date started: unknown
-# forked by: Robert McGuinn on 20190121
-# date that forked edits started: 20190121
+# author: Robert McGuinn
+# date started: 20190121
 # purpose: create a Minimum Viable Product (MVP) for export from the
 #       NOAA National Database for Deep Sea Corals and Sponges
 #       to Abby Benson for ingest to OBIS / GBIF
 
+##### packages #####
+library(tidyverse)
+library(dplyr)
+library(rmarkdown)
+library(knitr)
+library(flextable)
+library(googlesheets4)
+library(googlesheets)
+library(googledrive)
+library(openxlsx)
+library(scales)
+library(extrafont)
+loadfonts()
+library(RColorBrewer)
+
 ##### load NDB #####
 setwd("C:/rworking/deepseatools/indata")
-indata<-read_csv("DSCRTP_NatDB_20191217-0.csv", na = c("-999", "NA"))
+indata<-read_csv("DSCRTP_NatDB_20200408-1.csv", na = c("-999", "NA"))
 
 ##### filter the NDB #####
 filt <- indata %>%
-  filter(Flag == "0")
+  filter(Flag == "0") %>%
+  filter(AccessionID != 'NMNH_Smithsonian_update_Q2-2020_THourigan' |
+           CatalogNumber != '878942') %>%
+  filter(AccessionID != 'NMNH_Smithsonian_update_Q2-2020_THourigan' |
+           CatalogNumber != '878945')
 
 ##### removing records that do not have AphiaID #####
 filt <- filt %>% filter(is.na(filt$AphiaID) == F)
 
 ##### creating errdap_link field #####
-filt$references <- paste('https://ecowatch.ncddc.noaa.gov/erddap/tabledap/deep_sea_corals.csv?ShallowFlag%2CDatasetID%2CCatalogNumber%2CSampleID%2CCitation%2CRepository%2CScientificName%2CVernacularNameCategory%2CTaxonRank%2CIdentificationQualifier%2CLocality%2Clatitude%2Clongitude%2CDepthInMeters%2CDepthMethod%2CObservationDate%2CSurveyID%2CStation%2CEventID%2CSamplingEquipment%2CLocationAccuracy%2CRecordType%2CDataProvider&CatalogNumber=',
+filt$references <- paste('https://www.ncei.noaa.gov/erddap/tabledap/deep_sea_corals.csv?ShallowFlag%2CDatasetID%2CCatalogNumber%2CSampleID%2CCitation%2CRepository%2CScientificName%2CVernacularNameCategory%2CTaxonRank%2CIdentificationQualifier%2CLocality%2Clatitude%2Clongitude%2CDepthInMeters%2CDepthMethod%2CObservationDate%2CSurveyID%2CStation%2CEventID%2CSamplingEquipment%2CLocationAccuracy%2CRecordType%2CDataProvider&CatalogNumber=',
                           filt$CatalogNumber, sep = '')
 
 ##### fix Depth issues #####
@@ -101,12 +118,20 @@ recode_list <- list('specimen' = 'PreservedSpecimen',
 obis$basisOfRecord <- recode(obis$basisOfRecord, !!!recode_list)
 
 ##### write out file for submission #####
-today <- '20190122-2'
+today <- '20200415-1'
 version <- unique(filt$DatabaseVersion)
 setwd('C:/rworking/deepseatools/indata')
 obis %>%
   write.csv(paste(today,'_export_to_OBIS_DSCRTP_NDB_', version , '_RPMcGuinn', '.csv', sep = ''),
             row.names = FALSE)
+
+##### checking #####
+library(dplyr)
+
+x <- filt %>%
+  group_by(CatalogNumber) %>%
+  filter(n()>1)
+
 
 ##### DarwinCore crosswalk for other fields we might use later on #####
 # recordNumber = "TrackingID",
