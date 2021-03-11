02a2335f8841546a3728ee0fa5571005afd46ec4
Robert McGuinn <robert.mcguinn@noaa.gov>
2021-03-11 08:12:02 -0500

    working on arcgisbinding code

--- code/export_to_gis.R
+++ code/export_to_gis.R
@@ -1,7 +1,7 @@
 ##### header #####
 # author: Robert P. McGuinn
 # date_started: 20191121
-# purpose: transform point fram to spdf and feature-class
+# purpose: transform point frame to spdf and feature-class
 # input: x (dataframe) with coordinates
 # output: x_geo (spdf) | x_geo (feature-class)
 
@@ -12,20 +12,23 @@ arc.check_product()
 
 ##### __OR__ import the data frame from disk #####
 
-# x <- "20201001-4_NOAA_OER_EX1811_Oceano_Profundo_2018_2018"
-# setwd("C:/rworking/deepseatools/indata")
-# x <- read.csv(paste(x,'.csv', sep = ''), header = T)
+x <- "20201001-4_NOAA_OER_EX1811_Oceano_Profundo_2018_2018"
+setwd("C:/rworking/deepseatools/indata")
+x <- read.csv(paste(x,'.csv', sep = ''), header = T)
 
 ##### __OR__ create x from sub #####
 
-x <-
+# x <- sub
 
 ##### filter data #####
 
+# get rid of any missing Latitudes or Longitudes
 x <- sub %>% filter(Latitude != -999 | Longitude != -999)
+# make copy to turn into spatial points data frame.
 x_geo <- x
 
-##### create spdf #####
+##### create spatial points data fram #####
+
 coordinates(x_geo) <- c("Longitude", "Latitude")
 proj4string(x_geo) <- "+proj=longlat +ellps=WGS84 +datum=WGS84"
 
