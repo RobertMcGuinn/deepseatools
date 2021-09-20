65b7b4f922a59ea07e900a9ceb950cb1ffaf7a2b
Robert McGuinn <robert.mcguinn@noaa.gov>
2021-09-20 10:53:22 -0400

    hi density

--- code/rpubs_runner.R
+++ code/rpubs_runner.R
@@ -2,11 +2,11 @@
 setwd("C:/rworking/deepseatools/code")
 library(markdown)
 
-result <- rpubsUpload("20210916-0_DSBS16_HI_Analysis", "DSBS16_HI_Analysis.html")
+result <- rpubsUpload("20210920-0_DSBS16_HI_Analysis", "DSBS16_HI_Analysis.html")
 if (!is.null(result$continueUrl))
   browseURL(result$continueUrl) else stop(result$error)
 
 #
-updateResult <- rpubsUpload("20210916-0_DSBS16_HI_Analysis", "DSBS16_HI_Analysis.html", result$id)
+# updateResult <- rpubsUpload("20210916-0_DSBS16_HI_Analysis", "DSBS16_HI_Analysis.html", result$id)
 
 
