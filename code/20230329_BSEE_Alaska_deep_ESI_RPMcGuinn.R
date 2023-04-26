c27cb6b065c58c80f202abd563f1fe4790ee0f6a
Robert Mcguinn <1384419808113305@mil>
2023-04-26 16:20:30 -0400

    yo

--- code/20230329_BSEE_Alaska_deep_ESI_RPMcGuinn.R
+++ code/20230329_BSEE_Alaska_deep_ESI_RPMcGuinn.R
@@ -36,7 +36,6 @@ drive_auth(cache = ".secrets", email = "robert.mcguinn@noaa.gov")
 setwd("C:/rworking/deepseatools/indata")
 filename <- "DSCRTP_NatDB_20221213-0.csv"
 indata<-read_csv(filename,
-                 col_types = cols(.default = "c"),
                  locale = locale(encoding = 'latin9'),
                  na = c("-999", "NA"))
 
@@ -55,7 +54,6 @@ cook <- sf::st_read(paste(path,"BSEE_AOI_COOK_INLET_KODIAK_IS/BSEE_AOI_COOK_INLE
 
 ##### transform coral and sponge points to sf object #####
 points <- st_as_sf(filt, coords = c("Longitude", "Latitude"), crs = 4326)
-
 
 ##### **beaufort #####
 ##### buffer operation #####
@@ -169,6 +167,149 @@ filename <- '20230421_NDB_for_corals_sponges_version_20221213-0_subset_BSEE_AOI_
 write_csv(cook_corals_sponges,
           paste("c:/rworking/deepseatools/indata/",
                 filename))
+
+
+
+
+
+
+
+
+
+##### *****getting OBIS data for Beaufort sea *****#####
+library(spocc)
+beaufortobis <- st_transform(beaufort, crs = st_crs(points))
+bbox <- st_bbox(beaufortobis)
+bbox_matrix <- as.matrix(bbox)
+bbox = c(bbox_matrix[1,1],
+         bbox_matrix[2,1],
+         bbox_matrix[3,1],
+         bbox_matrix[4,1])
+
+out <- occ(geometry = bbox,
+           from = 'obis',
+           limit = 10000)
+obis <- as.data.frame(out$obis$data)
+obis2 <- obis %>% select(id, scientificNameID, recordNumber, latitude, longitude, phylum, family)
+
+##### turning the obis dataset into sf object  #####
+obis_points <- st_as_sf(obis2,
+                        coords = c("longitude", "latitude"),
+                        crs = 4326)
+
+##### transformation of CRS ####
+obis_points_transform <- st_transform(obis_points, crs = st_crs(beaufort))
+
+##### intersect the obis points with the aoi #####
+obis_points_intersect <- st_intersection(obis_points_transform, beaufort)
+
+##### use this intersection data to select from the original obis data #####
+intersects <- obis_points_intersect$id
+obis_beaufort_sea <- obis %>% filter(id %in% intersects)
+
+##### write csv #####
+write_csv(obis_beaufort_sea,
+          "c:/rworking/deepseatools/indata/20230426-0_obis_in_beaufort_sea_RPMcGuinn.csv")
+
+##### write shapefile #####
+setwd('c:/rworking/deepseatools/indata')
+st_write(obis_points_intersect,
+         "obis.shp",
+         delete_dsn = T)
+
+
+
+
+
+
+
+
+
+##### *****getting OBIS data for Chukchi sea *****#####
+library(spocc)
+chukchiobis <- st_transform(chukchi, crs = st_crs(points))
+bbox <- st_bbox(chukchiobis)
+bbox_matrix <- as.matrix(bbox)
+bbox = c(bbox_matrix[1,1],
+         bbox_matrix[2,1],
+         bbox_matrix[3,1],
+         bbox_matrix[4,1])
+
+out <- occ(geometry = bbox,
+           from = 'obis',
+           limit = 10000)
+obis <- as.data.frame(out$obis$data)
+obis2 <- obis %>% select(id, scientificNameID, recordNumber, latitude, longitude, phylum, family)
+
+##### turning the obis dataset into sf object  #####
+obis_points <- st_as_sf(obis2,
+                        coords = c("longitude", "latitude"),
+                        crs = 4326)
+
+##### transformation of CRS ####
+obis_points_transform <- st_transform(obis_points, crs = st_crs(chukchi))
+
+##### intersect the obis points with the aoi #####
+obis_points_intersect <- st_intersection(obis_points_transform, chukchi)
+
+##### use this intersection data to select from the original obis data #####
+intersects <- obis_points_intersect$id
+obis_chukchi_sea <- obis %>% filter(id %in% intersects)
+
+##### write csv #####
+write_csv(obis_chukchi_sea,
+          "c:/rworking/deepseatools/indata/20230426-0_obis_in_chukchi_RPMcGuinn.csv")
+
+##### write shapefile #####
+setwd('c:/rworking/deepseatools/indata')
+st_write(obis_points_intersect,
+         "obis_chukchi.shp",
+         delete_dsn = T)
+
+
+##### *****getting OBIS data for cook_kodiak *****#####
+library(spocc)
+cook_kodiakobis <- st_transform(cook, crs = st_crs(points))
+bbox <- st_bbox(cook_kodiakobis)
+bbox_matrix <- as.matrix(bbox)
+bbox = c(bbox_matrix[1,1],
+         bbox_matrix[2,1],
+         bbox_matrix[3,1],
+         bbox_matrix[4,1])
+
+out <- occ(geometry = bbox,
+           from = 'obis',
+           limit = 10000)
+obis <- as.data.frame(out$obis$data)
+obis2 <- obis %>% select(id, scientificNameID, recordNumber, latitude, longitude, phylum, family)
+
+##### turning the obis dataset into sf object  #####
+obis_points <- st_as_sf(obis2,
+                        coords = c("longitude", "latitude"),
+                        crs = 4326)
+
+##### transformation of CRS ####
+obis_points_transform <- st_transform(obis_points, crs = st_crs(cook))
+
+##### intersect the obis points with the aoi #####
+obis_points_intersect <- st_intersection(obis_points_transform, cook)
+
+##### use this intersection data to select from the original obis data #####
+intersects <- obis_points_intersect$id
+obis_cook_kodiak_sea <- obis %>% filter(id %in% intersects)
+
+##### write csv #####
+write_csv(obis_cook_kodiak_sea,
+          "c:/rworking/deepseatools/indata/20230426-0_obis_in_cook_kodiak_RPMcGuinn.csv")
+
+##### write shapefile #####
+setwd('c:/rworking/deepseatools/indata')
+st_write(obis_points_intersect,
+         "obis_cook_kodiak.shp",
+         delete_dsn = T)
+
+##### look at the DSCRTP data #####
+obis_cook_kodiak_sea %>% filter(dataset_id == 'f5a4799e-dc24-4807-89d9-01da47d52e3b') %>% pull(dataset_id)
 
 
 
