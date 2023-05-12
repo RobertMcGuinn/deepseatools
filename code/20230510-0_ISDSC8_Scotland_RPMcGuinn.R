02d40c97609b92254fde6c73b5bc41297c3abf3a
Robert Mcguinn <1384419808113305@mil>
2023-05-12 11:17:04 -0400

    yo

--- code/20230510-0_ISDSC8_Scotland_RPMcGuinn.R
+++ code/20230510-0_ISDSC8_Scotland_RPMcGuinn.R
@@ -3,6 +3,7 @@
 ## date started: 20230510
 ## purpose: ISDSC8 conference in Edinburgh, Scotland
 ## drive: https://drive.google.com/drive/folders/1e5-RzLCIOW4swlwGiemMnOoMgDyi_tal?usp=share_link
+## aprx: 20230511-0_ISDSC8_Scotland_RPMcGuinn
 
 ##### packages ####
 library(tidyverse)
@@ -26,43 +27,86 @@ big_palette <- c(palette1,palette2,palette3, palette4, palette5)
 
 ##### make depth box-plots #####
 ylower <- 0
-yupper <- 1000
+yupper <- 3500
 
 
 sub <- filt %>% filter(is.na(FishCouncilRegion) == F,
                        as.numeric(DepthInMeters) < yupper,
                        as.numeric(DepthInMeters) > ylower,
-                       VernacularNameCategory == 'gorgonian coral',
-                       TaxonRank == 'species')
+                       is.na(VernacularNameCategory) == F)
 
 list <- unique(sub$FishCouncilRegion)
+levels <- unique(sub$VernacularNameCategory)
+
+set.seed(9)
+color_lookup <- data.frame(VernacularNameCategory = levels,
+                           color = sample(big_palette, 18))
+
+sub <- left_join(sub, color_lookup, by = "VernacularNameCategory")
+sub$VernacularNameCategory <- as.factor(sub$VernacularNameCategory)
+
+# sub$color<-factor(sub$color)
+# levels(sub$color) <- color_lookup$VernacularNameCategory
+# levels(sub$color)
+
+my_colors <- c(
+"stony coral (branching)" =  "#FFFFFF",
+"stony coral (cup coral)" =  "#00E6A9",
+"black coral" =  "#000000",
+"gorgonian coral" = "#FF0000",
+"soft coral" =  "#00734C",
+"sea pen" = "#0000FF",
+"stoloniferan coral" = "#7F7F7F",
+"lace coral" = "#A80084",
+"lithotelestid coral" = "#FF00C5",
+"stony coral (unspecified)" = "#D1FF73",
+"gold coral" =  "#FFFF00",
+"sponge (unspecified)" = "#e0fff0",
+"demosponge" = "#00FFC5",
+"glass sponge" = "#C500FF",
+"homoscleromorph sponge" = "#FFFF00",
+"calcareous sponge" = "#55FF00",
+"other coral-like hydrozoan" = "#00FFFF",
+"alcyonacean (unspecified)" = "#D7B09E"
+)
+
+
 
 for(i in list){
   x <- sub %>% filter(FishCouncilRegion == i)
-  g <- ggplot(x, aes(reorder(ScientificName, DepthInMeters, FUN=median), as.numeric(DepthInMeters), fill = FishCouncilRegion)) +
+  g <- ggplot(x, aes(reorder(VernacularNameCategory, DepthInMeters, FUN=median), as.numeric(DepthInMeters), fill = VernacularNameCategory)) +
     geom_boxplot() +
     scale_y_reverse() +
     ylab("Depth (meters)") +
-    xlab("Genus") +
-    theme_bw(base_size = 10, base_family = "Cambria") +
-    theme(axis.text.x = element_text(angle = 90, hjust = 1, vjust = 0, face = 'italic'))
-  # geom_hline(yintercept = 2500, col = 'blue')
+    xlab("Vernacular Name Category") +
+    theme_bw(base_size = 13, base_family = "Cambria") +
+    theme(axis.text.x = element_text(angle = 30, hjust = 1, vjust = 1, face = 'italic'),
+          axis.title.y = element_text(margin = margin(r = 20))) +
+    scale_fill_manual(values = my_colors) +
+    labs(fill = "Vernacular Name Category")
 
 
-  set.seed(9)
-  g + scale_fill_manual(values = sample(big_palette))
-  #g + scale_color_manual(values = brewer.pal(12, "Paired")[c(10,9,8,7,6,5,4,3,2,1)])
   ggsave(paste("c:/rworking/deepseatools/images/ISDSC8_Scotland_RPMcGuinn/",
-               "20230510_",
+               "20230511_",
                "NatDB_",
                unique(sub$DatabaseVersion),
                '_',
                i,
                ".png",
                sep = ''),
-         width = 8,
+         width = 10,
          height = 6,
          units = "in")
 }
 
 
+
+##### create sf object from subset #####
+points <- st_as_sf(x, coords = c("Longitude", "Latitude"), crs = 4326)
+points <- points %>% select(VernacularNameCategory, ScientificName)
+
+##### ** check #####
+plot(points)
+
+##### export shapefile #####
+st_write(points, "C:/Users/Robert.Mcguinn/Documents/ArcGIS/Projects/20230511-0_ISDSC8_Scotland_RPMcGuinn/shapefiles/sub_geo.shp", delete_dsn = T)
