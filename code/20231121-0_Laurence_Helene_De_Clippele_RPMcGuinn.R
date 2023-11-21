8696c5de58d6d7f15eedafe922c0499619ac1a99
Robert Mcguinn <robert.mcguinn@noaa.gov>
2023-11-21 16:19:20 -0500

    tranforming and enhancing data

--- code/20231121-0_Laurence_Helene_De_Clippele_RPMcGuinn.R
+++ code/20231121-0_Laurence_Helene_De_Clippele_RPMcGuinn.R
@@ -51,11 +51,38 @@ points <- st_as_sf(sub2, coords = c("Longitude", "Latitude"), crs = 4326)
 st_write(points, "C:/rworking/deepseatools/indata/geo3.shp")
 
 ##### check #####
+# filt %>% filter(grepl('James Cook', Vessel)) %>%
+#   pull(Vessel) %>% table()
+#
+# filt %>% filter(grepl('James Cook', Vessel)) %>%
+#   pull(SurveyID) %>% table()
+#
+# filt %>% filter(grepl('Roberts', PI)) %>%
+#   pull(PI) %>% table()
+#
+# filt %>% filter(RecordType == 'literature') %>%
+#   pull(DataProvider) %>% unique()
 
-
-
-
-
+##### add information from DSCRTP schema #####
+sub2$Vessel <- "James Cook R/V"
+sub2$SurveyID <- "JC073"
+sub2$VehicleName <- "Holland 1"
+sub2$SamplingEquipment <- "ROV"
+sub2$Citation <- "https://www.frontiersin.org/articles/10.3389/fmars.2019.00184/full | https://www.bodc.ac.uk/resources/inventories/cruise_inventory/reports/jc073.pdf"
+sub2$WebSite <- "https://www.frontiersin.org/articles/10.3389/fmars.2019.00184/full | https://www.bodc.ac.uk/resources/inventories/cruise_inventory/reports/jc073.pdf | https://www.bodc.ac.uk/resources/inventories/cruise_inventory/report/11421/ | | http://rvinfobase.eurocean.org/spec/vessel.jsp?id=4140"
+sub2$RecordType <- "video observation"
+sub2$NavType <- "USBL"
+sub2$LocationAccuracy <- "20m"
+sub2$ObservationDate <- "2012-05-18" ## May 18–June 15, 2012
+sub2$Locality <- "Logachev Mound Province"
+sub2$Habitat <- sub2$Stones......Dropstone.
+sub2$DepthInMeters <- sub2$Depth.water..m.
+sub2$PI <- "De Clippele, Laurence Helene"
+sub2$PIAffiliation <- "University of Edinburgh, Edinburgh, United Kingdom"
+sub2$DataProvider <- "De Clippele et al. 2019"
+sub2$DataContact <- "De Clippele, Laurence Helene"
+sub2$Repository <- "National Oceanography Centre, British Oceanographic Data Centre"
+sub2 <- sub2 %>% mutate(CategoricalAbundance = ifelse(IndividualCount == 0, 'absent', 'present'))
 
 
 
