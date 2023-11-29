6e623156799e106b3a137913cb31acdea4d4727f
Robert Mcguinn <robert.mcguinn@noaa.gov>
2023-11-29 16:53:57 -0500

    initializing 121717

---
+++ code/121717.R
@@ -0,0 +1,27 @@
+##### Header #####
+## author: Robert P. McGuinn, robert.mcguinn@noaa.gov, rpm@alumni.duke.edu
+## startdate: 20231129
+## purpose: see redmine issue
+
+##### linkage #####
+## manual input here
+filename <- '121717' ## for this code .R
+github_path <- 'https://github.com/RobertMcGuinn/deepseatools/blob/master/code/'
+github_link <- paste(github_path, filename, sep = '')
+browseURL(github_link)
+redmine_path <- 'https://vlab.noaa.gov/redmine/issues/'
+## manual input here
+issuenumber <- filename
+redmine_link <- paste(redmine_path, issuenumber, sep = '')
+browseURL(redmine_link)
+
+##### packages #####
+library(tidyverse)
+library(sf)
+library(remotes)
+library(redmineR)
+
+##### load data #####
+setwd('c:/rworking/deepseatools/indata')
+sub <- read.csv('20231128-3_NewRecords-NMNH-Q1-2024_THourigan.csv', encoding = 'latin1')
+
