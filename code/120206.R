32fdb8cec853af389eadb69ef60c0b9c952d0b13
Robert Mcguinn <robert.mcguinn@noaa.gov>
2023-11-29 16:03:32 -0500

    initiating 120206

--- code/120206.R
+++ code/120206.R
@@ -1,7 +1,19 @@
 ##### Header #####
 ## author: Robert P. McGuinn, robert.mcguinn@noaa.gov, rpm@alumni.duke.edu
-## startdate: 20231128
-## purpose:
+## startdate:
+## purpose: see redmine issue
+
+##### linkage #####
+## manual input here
+filename <- '' ## for this code .R
+github_path <- 'https://github.com/RobertMcGuinn/deepseatools/blob/master/code/'
+github_link <- paste(github_path, filename, sep = '')
+browseURL(github_link)
+redmine_path <- 'https://vlab.noaa.gov/redmine/issues/'
+## manual input here
+issuenumber <- filename
+redmine_link <- paste(redmine_path, issuenumber, sep = '')
+browseURL(redmine_link)
 
 ##### packages #####
 library(tidyverse)
@@ -9,26 +21,18 @@ library(sf)
 library(remotes)
 library(redmineR)
 
-##### linkage #####
-## manual input here
-filename <- '121131' ## for this code .R
-github_path <- 'https://github.com/RobertMcGuinn/deepseatools/blob/master/code/'
-github_link <- paste(github_path, filename, ".R", sep = '')
-browseURL(github_link)
-redmine_path <- 'https://vlab.noaa.gov/redmine/issues/'
-issuenumber <- filename
-redmine_link <- paste(redmine_path, issuenumber, sep = '')
-browseURL(redmine_link)
-
 ##### load data #####
 setwd('c:/rworking/deepseatools/indata')
-sub <- read.csv('20231129-0_HURL_archive_records_SBingo_1972_2013_121131.csv')
+sub <- read.csv('20230823_Clarke_SH-18-12.csv')
 ## exported from original Excel file to (CSV UTF-8)
 
 ##### explore #####
+##### explore #####
+
 dim(sub)
 summary(sub)
 names(sub)
+table(sub$DataProvider)
 table(sub$SurveyID, useNA = 'always')
 table(sub$Vessel, useNA = 'always')
 table(sub$EventID, useNA = 'always')
@@ -45,27 +49,11 @@ filt %>%
   filter(grepl('Bingo', Reporter)) %>%
   pull(DatasetID) %>%
   table()
-
 filt %>%
-  filter(grepl('OET_NA134', DatasetID)) %>%
-  pull(TrackingID)
-
-dim(sub)
-length(setdiff(sub$SampleID, filt$SampleID))
-length(setdiff(sub$TrackingID, filt$TrackingID))
-
-
-length(unique(sub$SampleID))
-length(unique(sub$TrackingID))
-length(setdiff(unique(sub$SampleID), unique(filt$SampleID)))
-filt %>% filter(SampleID %in% sub$SampleID) %>% pull(SampleID) %>% length()
-filt %>%
-  filter(SurveyID %in% sub$SurveyID) %>%
-  pull(SurveyID) %>% unique()
+  filter(grepl('SH', DatasetID)) %>%
+  pull(DatasetID) %>%
+  table()
 
 
 
 
-
-
-##### taxonomic merge #####
