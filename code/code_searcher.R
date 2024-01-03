9d9dda9381420424d429dafa1e691a164c6d40f0
Robert Mcguinn <robert.mcguinn@noaa.gov>
2024-01-03 15:01:22 -0500

    vernacularnamecategory patch

--- code/code_searcher.R
+++ code/code_searcher.R
@@ -1,6 +1,6 @@
 ##### find file #####
 ## manual: edit string for x
-x <- 'header'
+x <- 'load'
 path <- 'C:/rworking/deepseatools/code'
 files<-list.files(path,
                   pattern=x,
@@ -11,7 +11,7 @@ files
 
 ##### choose and open #####
 ## manual input required: pick the number or number you want from the list presented
-y <- c(1)
+y <- c(2)
 path <- files[y]
 file.edit(path)
 
--- code/mod_vernacularnamecategory_patch.R
+++ code/mod_vernacularnamecategory_patch.R
@@ -19,12 +19,42 @@ library(redmineR)
 ##### grab all AphiaIDs #####
 aphiaIDs <- unique(filt$AphiaID)
 
-##### create a crosswalk
+##### additional AphiaIDs ######
+## these are where Aphia was -999 at version
+filt %>% filter(AphiaID == -999) %>%
+  pull(ScientificName) %>%
+  unique()
+
+1578714 # [1] "Bathypathes pseudoalternata"
+-999 # [2] "Leptogorgia porrecta"
+1521953 # [3] "Parantipathes pluma"
+-999 # [4] "Pseudothesea pailoloensis"
+-999 # [5] "Tribrachium symmetra"
+-999 # [6] "Atlantisella alenuihaha"
+-999 # [7] "Lepidisis cornucopia"
+1391784 # [8] "Kophobelemnon biflorum"
+1392576 # [9] "Lithophytum roseum"
+-999 # [10] "Chrysogorgia arbuscula"
+-999 # [11] "Halisarca membrana"
+-999 # [12] "Neopelta aberrans"
+-999 # [13] "Acromuricea hirtella"
+-999 # [14] "Acromuricea alatispina"
+1635628 # [15] "Iridogorgia densispicula"
+1635629 # [16] "Iridogorgia squarrosa"
+1647390 # [17] "Sibogagorgia californica"
+1514479 # [18] "Aphanostichopathes"
+1317063 # [19] "Callogorgia cracentis"
+
+
+
+
+##### create a crosswalk #####
 crosswalk <- filt %>%
-  group_by(AphiaID, VernacularNameCategory) %>%
+  group_by(AphiaID, VernacularNameCategory, ScientificName, VerbatimScientificName) %>%
   summarize(n=n())
 
 ##### check #####
+View(crosswalk)
 length(aphiaIDs)
 dim(crosswalk)
 length(aphiaIDs) - dim(crosswalk)
@@ -35,4 +65,7 @@ class(crosswalk$n)
 
 
 
+
+
+##### find all entries where crosswalk has n > 1 #####
 
