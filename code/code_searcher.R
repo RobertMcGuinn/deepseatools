08fb3a1e98ded6742caa2dbd8a91947731bb1a85
Robert Mcguinn <1384419808113305@mil>
2024-04-04 14:20:40 -0400

    yo

--- code/code_searcher.R
+++ code/code_searcher.R
@@ -1,6 +1,6 @@
 ##### find file #####
 ## manual: edit string for x
-x <- '20230828-0_rmd_quarterly_report_for_database_update'
+x <- 'taxonomy_patch'
 path <- 'C:/rworking/deepseatools/code'
 files<-list.files(path,
                   pattern=x,
@@ -9,10 +9,10 @@ files<-list.files(path,
 ## look at the list
 files
 
-
 ##### choose and open #####
-## manual input required: pick the number or number you want from the list presented
-y <- c(1)
+## manual input required: pick the number
+## or number you want from the list presented
+y <- c()
 path <- files[y]
 file.edit(path)
 
@@ -22,9 +22,19 @@ source(path)
 ##### clean up everything except filt ######
 rm(list=setdiff(ls(), c("filt")))
 
+##### find out the time files were edited #####
 
+# Get file information
+file_info <- lapply(files, file.info)
 
+# Extract modification time
+modification_times <- sapply(file_info, function(info) info$mtime)
 
+# Combine filenames with modification times
+file_info_df <- data.frame(File = basename(files), Modification_Time = modification_times)
+
+# Print the result
+print(file_info_df) ## files having a larger Modification_Time were created later in time.
 
 
 
