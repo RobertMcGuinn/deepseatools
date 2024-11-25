cec5485d70e80032437dc5c197e63ff60fabe582
Robert Mcguinn <1384419808113305@mil>
2024-11-25 11:37:33 -0500

    yo

--- code/127481-2.R
+++ code/127481-2.R
@@ -88,6 +88,10 @@ coralsfish <- bind_rows(corals, fish)
 coralsfish_cl <- coralsfish %>% filter(is.na(ScientificName) == F)
 coralsfish_cl <- coralsfish %>% filter(!(grepl('Unknown', ScientificName) | grepl('unknown', ScientificName)))
 
+##### check: put cf in front of every column name to differentiate in join operations #####
+# coralsfish_cl <- coralsfish_cl %>%
+#   rename_with(~ paste0("cf_", .))
+
 ##### check #####
 # table(coralsfish_cl$ScientificName, useNA = 'always')
 # coralsfish %>% filter(is.na(ScientificName) == T) %>% pull(ScientificName) %>% length()
@@ -103,7 +107,7 @@ coralsfish_cl$Timestamp_pos <- ymd_hms(coralsfish_cl$Timestamp)
 geology$Timestamp_pos <- ymd_hms(geology$Timestamp)
 
 ## Specify the tolerance window in seconds (e.g., 600 seconds for 10 minutes)
-tolerance_window <- 20
+tolerance_window <- 5
 
 ## Perform the fuzzy left join
 result <- difference_left_join(coralsfish_cl, geology,
@@ -114,10 +118,25 @@ result <- difference_left_join(coralsfish_cl, geology,
   slice_min(order_by = time_diff, n = 1, with_ties = FALSE) %>% # `with_ties = FALSE` ensures only one row per Timestamp_pos.x
   ungroup()
 
+coralsfish_cl$joiner_Timestamp_pos <- paste(coralsfish_cl$ScientificName, coralsfish_cl$Timestamp, sep = '_')
+result$joiner_Timestamp_pos.x <- paste(result$ScientificName, result$Timestamp.x, sep = '_' )
+
 result <-
-  left_join(coralsfish_cl, result, by = c("Timestamp_pos" = "Timestamp_pos.x"))
+  left_join(coralsfish_cl, result, by = c("joiner_Timestamp_pos" = "joiner_Timestamp_pos.x"))
 
 ##### check #####
+result %>% group_by(ScientificName.x, ScientificName.y) %>%
+  summarize(n=n(), .groups = 'drop') %>% View()
+
+sort(names(result))
+
+result %>% group_by(time_diff, Timestamp_pos, Timestamp_pos.y) %>%
+  summarize(n=n()) %>% arrange(desc(time_diff)) %>% View()
+
+result %>% group_by(DominantSubstrate.x, DominantSubstrate.y, CMECSGeoform1, CMECSGeoform2) %>%
+  summarize(n=n(), .groups = "drop") %>% arrange(desc(n)) %>%
+  print(n=100)
+
 sum(is.na(result$Timestamp_pos))
 sort(names(coralsfish_cl))
 sort(names(result))
@@ -126,7 +145,6 @@ result %>% group_by(DominantSubstrate,
                     DominantSubstrate.x,
                     DominantSubstrate.y,) %>%
   summarize(n=n()) %>% View()
-
 
 result %>% group_by(AnnotatorResponse,
                     AnnotatorResponse.x,
@@ -207,43 +225,36 @@ dscrtp_export <- result %>%
   mutate(
     ImageFilePath = paste(MediaId, MediaName, Frame, sep = ' | '), ## this will need to be adapted when we get the media in house
     SampleAreaInSquareMeters = SampleAreaInSquareMeters.x,
-
     ScientificName = ScientificName.x,
-    Morphospecies = Morphospecies.x,
+    Morphospecies = paste(Morphospecies.x, Color.x, sep = ' | '),
     IdentifiedBy = paste(IdentifiedBy, ReviewedBy, set = ' | '),
     IdentificationDate = IdentificationDate,
     IdentificationComments = paste(IdentificationComments, AnnotatorResponse, OccurrenceComments.x, sep = ' | '),
     OccurrenceComments = paste(OccurrenceComments.x, TypeOfInjury.x, sep = ' | '),
     Condition = Condition.x, # needs transformation
-
     Longitude = Longitude,
     Latitude = Latitude,
     ObservationDate = Timestamp,
     MinimumDepthInMeters = NavDepth,
     MaximumDepthInMeters = NavDepth,
-
-    CategoricalAbunance = paste(CategoricalAbundance.x, FishCategoricalAbundance.x, sep = ' | '), ## mixed up for fish
+    CategoricalAbundance = paste(CategoricalAbundance.x, FishCategoricalAbundance.x, sep = ' | '), ## mixed up for fish, needs transformation
     IndividualCount = IndividualCount.x,
     Cover = PercentCover.x,
-
-    MaximumSize = paste(MaximumSizeHeight.x, MaximumSizeWidth.x, FishTotalLength.x, sep = ' | '), ## mixed up for fish
-    MinimumSize = paste(MinimumSizeHeight.x, MinimumSizeWidth.x, FishTotalLengh.x, sep = ' | '), ## mixed up for fish
-
+    MaximumSize = paste(MaximumSizeHeight.x, MaximumSizeWidth.x, FishTotalLength.x, sep = ' | '), ## mixed up for fish, needs transformation
+    MinimumSize = paste(MinimumSizeHeight.x, MinimumSizeWidth.x, FishTotalLength.x, sep = ' | '), ## mixed up for fish, needs transformation
     CMECSGeoForm = paste(CMECSGeoform1, CMECSGeoform2, sep = ' | '),
     Habitat = paste(CMECSGeoform1, CMECSGeoform2, DominantSubstrate, sep = ' | '),
     Salinity = Salinity.x,
     Temperature = Temperature.x,
-
     SurveyID = CruiseId,
-    EventID = paste(DiveID,TransectID, sep = '-'),
-
+    EventID = paste(DiveId,TransectId.x, sep = '-'),
     BboxHeight = BboxHeight.x,
     BboxWidth = BboxWidth.x,
     BboxX = BboxX.x,
     BboxY = BboxY.x)
 
 
-##### paste information to OccurrenceComments #####
+## paste information to OccurrenceComments
 dscrtp_export$OccurrenceComments <- paste(dscrtp_export$OccurrencComments,
                                           'Heights measured using Tator software', sep = ' | ')
 
@@ -251,13 +262,10 @@ dscrtp_export$OccurrenceComments <- paste(dscrtp_export$OccurrenceComments,
                                           'Proportion of injury: ', dscrtp_export$Proportion.Of.Injury,
                                           'Type of injury: ', dscrtp_export$Type.Of.Injury, sep = ' | ')
 
-##### paste transect information into to EventID
-dscrtp_export$EventID <- paste(dscrtp_export$EventID, dscrtp_export$TransectID, sep = '-')
+## create Sample ID from pre-tranformed ObservationDate
+dscrtp_export$SampleID <- paste(dscrtp_export$ObservationDate, dscrtp_export$TatorId, sep = '_') #pre-transformed, see below for transformation.
 
-#### create Sample ID from pre-tranformed ObservationDate
-dscrtp_export$SampleID <- dscrtp_export$ObservationDate #pre-transformed, see below for transformation.
-
-##### transforming date and time #####
+## transforming date and time
 split_position = 10
 date <- substr(dscrtp_export$ObservationDate, 1, split_position)
 time <- substr(dscrtp_export$ObservationDate, split_position + 2, nchar(dscrtp_export$ObservationDate))
@@ -267,19 +275,50 @@ dscrtp_export$ObservationTime <- time
 ## get rid of everything after the '+' in the ObservationTime
 dscrtp_export$ObservationTime <- sub("\\+.*", "", dscrtp_export$ObservationTime)
 
-##### get just the DSCRTP fields out #####
+## get just the DSCRTP fields out
 dscrtp_fields <- intersect(names(dscrtp_export), s$FieldName)
 
-##### EXPORT TO DSCRTP, whittle down to the only the variables needed and create the export #####
 ## isolate the DSCRTP fields
 dscrtp_export_x <- dscrtp_export[,dscrtp_fields]
 
+## NOTE: These are the binary variables for habitat
+# BedrockMegaclast
+# BedrockMegaclast.x
+# BedrockMegaclast.y
+# BoulderCobble
+# BoulderCobble.x
+# BoulderCobble.y
+# CoralPresence.x
+# CoralPresence.y
+# CoralSubstrate
+# CoralSubstrate.x
+# CoralSubstrate.y
+# SandMud
+# SandMud.x
+# SandMud.y
+# ShellSubstrate
+# ShellSubstrate.x
+# ShellSubstrate.y
+
 ##### check #####
 dscrtp_export_x %>% pull(ObservationTime) %>% table(useNA = 'always')
+dscrtp_export_x %>% pull(ObservationDate) %>% table(useNA = 'always')
+dscrtp_export_x %>% pull(SampleID) %>% unique() %>% length()
+dscrtp_export_x %>% pull(SurveyID) %>% unique()
+dscrtp_export_x %>% pull(EventID) %>% unique()
+dscrtp_export_x %>% pull(Habitat) %>% unique()
+dscrtp_export_x %>% pull(CMECSGeoForm) %>% unique()
+dscrtp_export_x %>% pull(MinimumSize) %>% unique()
+dscrtp_export_x %>% pull(MaximumSize) %>% unique()
+dscrtp_export_x %>% pull(IndividualCount) %>% unique()
+dscrtp_export_x %>% pull(CategoricalAbundance) %>% unique()
+dscrtp_export_x %>% pull(Condition) %>% unique()
+dscrtp_export_x %>% pull(Condition) %>% table(useNA = 'always')
+s %>% filter(FieldName == 'Condition') %>% pull(ValidValues)
 
-## write the file to disk
+##### write the file to disk #####
 write.csv(dscrtp_export_x,
-          'c:/rworking/deepseatools/indata/20240410-0_TATOR_DSCRTP_export_RPMcGuinn.csv',
+          'c:/rworking/deepseatools/indata/20241125-0_TATOR_DSCRTP_export_RPMcGuinn.csv',
           row.names = F)
 
 
