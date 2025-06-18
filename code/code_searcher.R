6fcaa32c69a2af4521887e0607235d4d1be65c09
RobertMcGuinn <robert.mcguinn@noaa.gov>
2025-06-18 14:34:24 -0400

    listing folders from Google Drive

--- code/code_searcher.R
+++ code/code_searcher.R
@@ -1,6 +1,6 @@
 ##### find file #####
 ## manual: edit string for x
-x <- '143699'
+x <- 'google'
 path <- 'C:/rworking/deepseatools/code'
 files<-list.files(path,
                   pattern=x,
@@ -12,7 +12,7 @@ files
 ##### choose and open #####
 ## manual input required: pick the number
 ## or number you want from the list presented
-y <- c(1)
+y <- c(2)
 this <- files[y]
 file.edit(this)
 
--- code/googledrive_tools.R
+++ code/googledrive_tools.R
@@ -2,80 +2,21 @@
 # Author: Robert McGuinn
 # Email: rpm@alumni.duke.edu
 # Purpose: Interacting with Google Drive using R
-##### installation/loading of packages #####
-#install.packages('pandoc')
-library(pandoc)
-#install.packages("pacman")
-library(pacman)
-#pacman::p_load(captioner, bundesligR)
-library(captioner, bundesligR)
-#install.packages("beanplot")
-library(beanplot)
-#install.packages("stringr")
-library(stringr)
-#install.packages("knitr")
-library(knitr)
-#install.packages("tidyr")
-library(tidyr)
-#install.packages("sp")
-library(sp)
-#install.packages("maptools")
-library(maptools)
-#install.packages("maps")
-library(maps)
-#install.packages("reshape")
-library(reshape)
-#install.packages("reshape2")
-library(reshape2)
-#install.packages("psych")
-library(psych)
-#install.packages("ggplot2")
-library(ggplot2)
-#install.packages("data.table")
-library(data.table)
-#install.packages("dplyr")
-library(dplyr)
-#install.packages("car")
-library(car)
-#install.packages("gdata")
-library(gdata)
-#install.packages("digest")
-library(digest)
-#install.packages("rgdal")
-library(rgdal)
-#install.packages("ggmap")
-library(ggmap)
-#install.packages("rerddap")
-library(rerddap)
-#install.packages("raster")
-library(raster)
-#install.packages("rworldxtra")
-library(rworldxtra)
-#install.packages("ggrepel")
-library(ggrepel)
-#install.packages("xtable")
-library(xtable)
-library(taxize)
-library(rgdal)
-library(dplyr)
-#install.packages("tidyverse")
+##### packages #####
 library(tidyverse)
-#install.packages("extrafont")
-library(extrafont)
-library(RColorBrewer)
-library(googlesheets)
 library(googledrive)
 library(googlesheets4)
 
 ##### download Google Sheet version of schema for use in R  documents #####
 # Register and download Google Sheet
+## MANUAL: double check this id is the latest one
 s <- gs_key('1YDskzxY8OF-34Q8aI04tZvlRbhGZqBSysuie39kYHoI')
 schema<- gs_read(s)
 #gs_browse(s)
 
 # write out to CSV
 setwd("C:/rworking/digs/indata")
-write.csv(schema, "2018_DSCRTP_Schema.csv")
+write.csv(schema, "2018_DSCRTP_Schema.csv") # manual name change
 
 ##### reading in sheet googlesheets4 #####
 s <- read_sheet('1YDskzxY8OF-34Q8aI04tZvlRbhGZqBSysuie39kYHoI')
@@ -208,5 +149,135 @@ rmarkdown::render('20180806_0_Status_Update_RPMcGuinn.rmd', output_format=gdoc()
 #or just render to local (html and doc are controlled in YAML header)
 setwd("C:/rworking/digs/code")
 rmarkdown::render('20180806_0_Status_Update_RPMcGuinn.rmd')
+
+
+
+##### **** folder listings on google drive ***** #####
+##### authorizations #####
+gs4_auth(cache = ".secrets", email = "robert.mcguinn@noaa.gov")
+drive_auth(cache = ".secrets", email = "robert.mcguinn@noaa.gov")
+
+##### get folder by name #####
+# Get folder by name
+folder <- drive_get("Team Coordination")
+# List contents of the folder
+drive_ls(folder)
+
+##### list information in a shared drive #####
+id <- "0AAbgBr_cP0q1Uk9PVA"
+shared_drive_id <- as_id(id)
+drive_ls(path = shared_drive_id,
+         corpus = "drive",
+         shared_drive = shared_drive_id)
+
+##### copy between my_drive and shared drive #####
+source_folder <- drive_get("testing_shared")
+
+## set Shared Drive ID
+id <- "0AAbgBr_cP0q1Uk9PVA"
+shared_drive_id <- as_id(id)
+
+## Create a folder and a 2nd level in the shared drive
+target_folder <- drive_mkdir("test2", path = shared_drive_id)
+target_folder_L2 <- drive_mkdir("test3", path = target_folder)
+
+# List all files in source folder
+files_to_copy <- drive_ls(source_folder)
+
+# Copy each file to the target folder
+for (i in seq_len(nrow(files_to_copy))) {
+  drive_cp(
+    file = files_to_copy[i, ],
+    path = target_folder
+  )
+}
+
+## delete a folder from shared drive
+folder <- drive_get("test2", shared_drive = shared_drive_id)
+drive_rm(folder)
+
+
+##### list Google folders as a tree (with files) #####
+# Function to check if a file is a folder
+is_folder <- function(file) {
+  file$drive_resource[[1]]$mimeType == "application/vnd.google-apps.folder"
+}
+
+# Recursive function to print folder tree (includes root)
+list_drive_tree <- function(folder = NULL, indent = 0, root_shown = FALSE) {
+  # Show root name on first call
+  if (!root_shown) {
+    root_name <- if (is.null(folder)) "My Drive" else folder$name
+    cat(strrep("  ", indent), "- ", root_name, "\n", sep = "")
+    root_shown <- TRUE
+  }
+
+  # List contents of the current folder
+  contents <- drive_ls(path = folder)
+
+  # If empty, stop here
+  if (nrow(contents) == 0) return()
+
+  # Loop through contents
+  for (i in seq_len(nrow(contents))) {
+    item <- contents[i, ]
+    is_folder_item <- item$drive_resource[[1]]$mimeType == "application/vnd.google-apps.folder"
+
+    # Print name with indentation
+    cat(strrep("  ", indent + 1), "- ", item$name, "\n", sep = "")
+
+    # If it's a folder, recurse
+    if (is_folder_item) {
+      list_drive_tree(item, indent + 1, root_shown = TRUE)
+    }
+  }
+}
+
+# Start from a specfic folder (My Drive)
+folder <- drive_get("Custom_Analyses")
+list_drive_tree(folder)
+
+##### list folders as tree (sans files) #####
+
+# Recursive folder tree printer including the root
+list_drive_folders_tree <- function(folder = NULL, indent = 0) {
+  # If folder is NULL, start from My Drive root
+  if (is.null(folder)) {
+    folder_name <- "My Drive"
+    cat(strrep("  ", indent), "- ", folder_name, "\n", sep = "")
+    contents <- drive_ls(path = NULL)
+  } else {
+    folder_name <- folder$name
+    cat(strrep("  ", indent), "- ", folder_name, "\n", sep = "")
+    contents <- drive_ls(path = folder)
+  }
+
+  # Filter only folders
+  if (nrow(contents) > 0) {
+    folders <- contents[grepl("application/vnd.google-apps.folder",
+                              sapply(contents$drive_resource, `[[`, "mimeType")), ]
+  } else {
+    return()
+  }
+
+  # Recurse into each subfolder
+  for (i in seq_len(nrow(folders))) {
+    list_drive_folders_tree(folders[i, ], indent + 1)
+  }
+}
+
+
+# Start from a specfic folder (My Drive)
+folder <- drive_get("Custom_Analyses")
+list_drive_folders_tree(folder)
+
+
+
+
+
+
+
+
+
 
 
