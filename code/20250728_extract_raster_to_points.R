dd2980708a6f98f80d4ebf493faa6d8b8b7fc026
RobertMcGuinn <robert.mcguinn@noaa.gov>
2025-07-29 00:14:35 -0400

    yo

--- code/20250728_extract_raster_to_points.R
+++ code/20250728_extract_raster_to_points.R
@@ -31,39 +31,7 @@ pts <- vect(points,
 ##### extract raster values at point locations #####
 vals <- terra::extract(r, pts)
 
-##### join extracted values to the original points data frame #####
-points_with_vals_joined <- points %>%
-  mutate(ID = row_number()) %>%
-  left_join(vals, by = "ID")
-
-##### map it with ggplot2 #####
-## Calculate bounding box around points (shifted longitudes)
-x_range <- range(points_with_vals_joined$Lon_shifted, na.rm = TRUE)
-y_range <- range(points_with_vals_joined$Latitude, na.rm = TRUE)
-
-ggplot() +
-  geom_raster(data = r_df, aes(x = x_shifted, y = y, fill = value)) +
-  geom_point(data = points_with_vals_joined, aes(x = Lon_shifted, y = Latitude), color = "red", size = 2) +
-  scale_fill_viridis_c(na.value = "transparent") +
-  scale_x_continuous(
-    limits = c(0, 360),  # Keep full range for breaks and labels
-    breaks = seq(0, 360, 60),
-    labels = function(x) ifelse(x > 180, x - 360, x)
-  ) +
-  coord_fixed(
-    xlim = x_range,
-    ylim = y_range,
-    expand = FALSE
-  ) +
-  theme_minimal() +
-  labs(fill = "Raster Value", x = "Longitude", y = "Latitude",
-       title = "Zoomed Map to Points Extent")
-
-
-
-##### map it with leaflet #####
-library(leaflet)
-
+##### prep data #####
 # Fix column name before joining
 colnames(vals)[2] <- "value"
 
@@ -71,6 +39,7 @@ points_with_vals_joined <- points %>%
   mutate(ID = row_number()) %>%
   left_join(vals, by = "ID")
 
+##### create interactive map #####
 # Leaflet interactive map
 leaflet(data = points_with_vals_joined) %>%
   addProviderTiles(providers$Esri.OceanBasemap) %>%
