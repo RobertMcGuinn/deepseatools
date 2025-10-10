7c06e39e83bb1ceca46fd16eaf2ed436310de6b3
RobertMcGuinn <robert.mcguinn@noaa.gov>
2025-10-10 16:12:44 -0400

    release to obis

--- code/code_searcher.R
+++ code/code_searcher.R
@@ -2,7 +2,7 @@
 library(tidyverse)
 ##### find file #####
 ## manual: edit string for x
-x <- 'ssh'
+x <- 'git'
 path <- 'C:/rworking/deepseatools/code'
 files<-list.files(path,
                   pattern=x,
--- code/new_project_git_setup.R
+++ code/new_project_git_setup.R
@@ -4,3 +4,11 @@ system("git config --global user.email 'robert.mcguinn@noaa.gov'")
 
 use_git()
 use_github()
+
+usethis::github_token()
+
+use_sitrep()
+sitrep()
+usethis::sitrep()
+usethis::proj_sitrep()
+
