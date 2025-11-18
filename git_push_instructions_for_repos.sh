0dcbe554674110c7dc7994e2aa75abea446e107b
RobertMcGuinn <robert.mcguinn@noaa.gov>
2025-11-18 11:46:20 -0500

    yo

--- git_push_instructions_for_repos.sh
+++ git_push_instructions_for_repos.sh
@@ -1,5 +1,5 @@
 ## pushing to vlab gitlab (to branch: deepseatools)
-git push -u vlab_gitlab master:deepeatools
+git push -u vlab_gitlab master:deepseatools
 
 ## pushing to gitlab (to branch: master)
 git push gitlab
