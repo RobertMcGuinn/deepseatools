b4f2078480613a935499c08ae7ff62235341d083
RobertMcGuinn <robert.mcguinn@noaa.gov>
2026-01-06 13:30:36 -0500

    python test

---
+++ code/test.py
@@ -0,0 +1,12 @@
+print("Python is working!")
+
+# Basic calculation
+x = 2 + 3
+print("2 + 3 =", x)
+
+# Test a standard library
+import math
+print("Square root of 16 =", math.sqrt(16))
+
+library(reticulate)
+py_config()
