b2b575f9c49e47d85a50ab622fbf1e9a9aa918a0
RobertMcGuinn <robert.mcguinn@noaa.gov>
2026-01-26 14:41:23 -0500

    report datasetID dashboard 20260121-0

--- code/dst_tool_code_searcher.R
+++ code/dst_tool_code_searcher.R
@@ -2,7 +2,7 @@
 library(tidyverse)
 ##### find file #####
 ## manual: edit string for x
-x <- 'rmd_datasetid_cruise'
+x <- 'rmd_datasetid_cruise_no_leaflet'
 path <- 'C:/rworking/deepseatools/code'
 files<-list.files(path,
                   pattern=x,
@@ -14,7 +14,7 @@ files
 ##### choose and open #####
 ## manual input required: pick the number
 ## or number you want from the list presented
-y <- c(2)
+y <- c(1)
 this <- files[y]
 file.edit(this)
 
@@ -50,10 +50,26 @@ file_info_df <- file_info_df %>% arrange(Modification_Time)
 print(file_info_df) ## files having a larger Modification_Time were created later in time.
 
 ##### clean up everything except filt ######
-rm(list=setdiff(ls(), c("filt")))
+# rm(list=setdiff(ls(), c("filt")))
 
+### Function to clear environment except for specific objects
+keep_only <- function(keep_list) {
+  # 1. Get all objects in the Global Environment
+  all_vars <- ls(envir = .GlobalEnv)
 
+  # 2. Identify which ones are NOT in our keep list
+  # We also make sure the function itself doesn't try to delete itself while running
+  to_delete <- setdiff(all_vars, c(keep_list, "keep_only"))
 
+  # 3. Remove them explicitly from the Global Environment
+  rm(list = to_delete, envir = .GlobalEnv)
+
+  message(paste("Environment cleared. Kept:", paste(keep_list, collapse = ", ")))
+}
+
+### How to use it:
+keep_only("filt")
+# keep_only(c("filt", "my_data", "model_results"))
 
 
 
