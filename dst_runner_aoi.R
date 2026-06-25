9caf04f88360e6a5afc0b489ba1f84997949f8c3
Robert Mcguinn <robert.mcguinn@noaa.gov>
2026-06-25 16:37:39 -0400

    yo

--- dst_runner_aoi.R
+++ dst_runner_aoi.R
@@ -1,7 +1,7 @@
 library(rmarkdown)
 render(
   input = "code/dst_report_aoi.Rmd",       # The name of your saved RMarkdown file
-  output_file = "dst_report.html",        # Your desired output filename
+  output_file = "dst_report_aoi.html",        # Your desired output filename
   output_dir = "reports"             # The target folder
 )
 
