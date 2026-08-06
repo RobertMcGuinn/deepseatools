acf0e4c13c7bc180ea28696ceec85795271d1e87
Robert Mcguinn <robert.mcguinn@noaa.gov>
2026-08-06 19:12:15 -0400

    revamp deepseatools

---
+++ code/dst_tool_posit_connect.R
@@ -0,0 +1,39 @@
+library(connectapi)
+library(bookdown)
+library(rsconnect)
+
+# 1. Render your book locally (outputs to docs/)
+bookdown::render_book("index.Rmd")
+
+# 2. Connect to server
+client <- connect()
+
+# 3. Target the "docs" folder and declare index.html as the entry point
+rsconnect::writeManifest(
+  appDir = "docs",
+  contentCategory = "site",
+  appPrimaryDoc = "index.html"
+)
+
+# 4. Bundle ONLY the docs folder
+bundle <- bundle_dir("docs")
+
+# 5. Deploy to Posit Connect (updating your existing GUID)
+deploy_obj <- deploy(
+  client,
+  bundle,
+  guid = "82b540a6-3534-4520-a5e0-c8e218443a22",
+  name = "deep-sea-code-book"
+)
+
+# 6. Stream build logs until finished
+poll_task(deploy_obj)
+
+# Get the URL directly (no $get_content() needed!)
+live_url <- deploy_obj$get_url()
+
+# Print it to your console
+print(live_url)
+
+# Open your live site in your browser
+browseURL(live_url)
