65f6c004208132119d3426c40f1652643fcee629
Robert Mcguinn <robert.mcguinn@noaa.gov>
2026-08-06 19:32:23 -0400

    update to PositConnect

--- code/dst_tool_posit_connect.R
+++ code/dst_tool_posit_connect.R
@@ -1,3 +1,27 @@
+##### Header #####
+## author: Robert P. McGuinn, robert.mcguinn@noaa.gov, rpm@alumni.duke.edu
+## startdate: 20260806
+## purpose: publish bookdown on PositConnect
+
+##### parameters #####
+##### linkage #####
+current_file <- rstudioapi::getSourceEditorContext()$path
+filename <- basename(current_file)
+print(filename)
+github_path <- 'https://github.com/RobertMcGuinn/deepseatools/blob/master/code/'
+github_link <- paste(github_path, filename, sep = '')
+# browseURL(github_link)
+redmine_path <- 'https://vlab.noaa.gov/redmine/issues/'
+issuenumber <- filename
+redmine_link <- paste(redmine_path, issuenumber, sep = '')
+# browseURL(redmine_link)
+
+
+##### packages #####
+library(tidyverse)
+
+##### section #####
+
 library(connectapi)
 library(bookdown)
 library(rsconnect)
